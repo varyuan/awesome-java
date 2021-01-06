93f64fb27874a4d15c0a796e86aa4c99b1c5203c
varyuan <varyuan@qq.com>
2021-01-06 21:19:24 +0800

    日志文件名字增加POD_IP

--- src/main/java/com/varyuan/awesome/web/SimpleController.java
+++ src/main/java/com/varyuan/awesome/web/SimpleController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 
 @RestController
@@ -15,16 +16,25 @@ public class SimpleController {
     private static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
 
     // curl localhost:8080/awesome/health
+    // 健康检查
     @GetMapping("/health")
     public String healthCheck() {
         return "health check success";
     }
 
     // curl localhost:8080/awesome/clientIp
+    // 返回请求头 X-Forwarded-For 的值
     @GetMapping("/clientIp")
     public String clientIp(HttpServletRequest httpServletRequest) {
         String headerVal = httpServletRequest.getHeader("X-Forwarded-For");
         logger.info("/clientIp , from {}", headerVal);
         return headerVal;
+    }
+
+    // curl localhost:8080/awesome/env
+    // 返回服务器所有的环境变量
+    @GetMapping("/env")
+    public Map<String, String> env() {
+        return System.getenv();
     }
 }
